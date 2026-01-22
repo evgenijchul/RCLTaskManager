b4f98133357274dc7faad79ebdda1a68af32d96f
Chulkov Evgenij <evgenij.chulkov@gmail.com>
2026-01-22 18:00:07 +0100

    Update ESLint configuration to disable no-inline-styles rule for dynamic color styles

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    // This app uses a theme object, so color styles are dynamic.
+    'react-native/no-inline-styles': 'off',
+  },
 };
